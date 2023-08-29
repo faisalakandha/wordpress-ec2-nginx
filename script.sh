32dee2bce2880ac31fc8876cb8f881283558ca55
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-08-29 03:13:29 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 sudo apt upgrade
 sudo apt update
 #Installing Essential Requirements
-sudo apt install nginx mysql-server php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
+sudo apt install -y nginx mysql-server php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip 
