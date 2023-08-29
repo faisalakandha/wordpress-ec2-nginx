8282ad72ab6d6b51646a37903d2e3e2fdd5ec464
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-08-29 03:11:15 -0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Machine Update
+sudo apt upgrade
+sudo apt update
+#Installing Essential Requirements
+sudo apt install nginx mysql-server php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
