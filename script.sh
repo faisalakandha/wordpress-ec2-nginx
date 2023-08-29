95dafee1412cd6a734cdb5012d94d01521d52ea4
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-08-29 12:47:54 +0000

    Script Changed

--- script.sh
+++ script.sh
@@ -1,48 +1,69 @@
-#!/bin/sh
-# Machine Update
+#!/bin/bash
+
+# Update system packages
+sudo apt update
 sudo apt upgrade -y
-sudo apt update -y
-#Installing Essential Requirements
-sudo apt install nginx mysql-server php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
-#Configuring nginx
+
+# Install Nginx
+sudo apt install nginx -y
+
+# Start Nginx
+sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo systemctl status nginx
-# Path to the sites-available directory
-sites_available_dir="/etc/nginx/sites-available"
 
-# Name of the configuration file
-config_file="wordpress.conf"
+# Install MySQL
+sudo apt install mysql-server -y
 
-# Content of the configuration file
-config_content=$(cat << EOF
+# Secure MySQL installation
+sudo mysql_secure_installation
+
+# Install PHP and required extensions
+sudo apt install php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+
+# Configure PHP-FPM
+sudo systemctl start php7.4-fpm
+sudo systemctl enable php7.4-fpm
+
+# Create a database for WordPress
+sudo mysql -u root -p -e "CREATE DATABASE wordpress;"
+sudo mysql -u root -p -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'wppassword';"
+sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';"
+sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
+
+# Download and configure WordPress
+sudo mkdir /var/www/html/wordpress
+cd /var/www/html/wordpress
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+sudo cp -r wordpress/* .
+sudo rm -rf wordpress latest.tar.gz
+sudo chown -R www-data:www-data /var/www/html/wordpress
+
+# Create Nginx virtual host configuration for WordPress
+sudo tee /etc/nginx/sites-available/wordpress <<EOF
 server {
+    listen 80;
+    server_name speckbridge.com www.speckbridge.com;
     root /var/www/html/wordpress;
-    index index.php index.html index.htm;
-    server_name example.com www.example.com;
-
-    client_max_body_size 100M;
+    index index.php;
 
     location / {
         try_files \$uri \$uri/ /index.php?\$args;
     }
 
-    location ~ \.php\$ {
+    location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     }
 }
 EOF
-)
 
-# Create the configuration file
-echo "$config_content" | sudo tee "$sites_available_dir/$config_file" > /dev/null
+# Enable the Nginx virtual host configuration
+sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
 
-echo "Created $config_file in $sites_available_dir"
-
-#Check nginx status
+# Test Nginx configuration and reload
 sudo nginx -t
+sudo systemctl reload nginx
 
-#Enable MySQL
-systemctl start mysql
-systemctl enable mysql
+echo "WordPress installation completed!"
